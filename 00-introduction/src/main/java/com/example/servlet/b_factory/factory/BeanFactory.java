8203f7605d158b8d091944d1a9f471478026e40d
xmxZzzz <1019566174@qq.com>
2022-02-10 14:26:48 +0800

    调整细节

--- 00-introduction/src/main/java/com/example/servlet/b_factory/factory/BeanFactory.java
+++ 00-introduction/src/main/java/com/example/servlet/b_factory/factory/BeanFactory.java
@@ -8,7 +8,7 @@ import com.example.servlet.b_factory.dao.impl.DemoOracleDaoImpl;
  * @ClassName: BeanFactory
  * @Author: guoyy
  * @Description: 使用静态工厂模式，通过手动切换返回的实例实现DemoDao
- * 问题：当缺失文件时，编译都不无法通过
+ * 问题：当缺失文件时，编译都无法通过
  * 本质：强耦合
  * @Date: 2021/12/11 下午8:21
  * @Version: 1.0
